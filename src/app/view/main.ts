7d76d76a4f691da2b4d4fb1c14fe03db1a9e1f9c
jayphbee <jayphbee@163.com>
2018-06-05 17:07:56 +0800

    wallet creation, import and export mnemonic words

--- src/app/view/main.ts
+++ src/app/view/main.ts
@@ -12,6 +12,7 @@ import { open, popNew } from '../../pi/ui/root';
 import { Forelet } from '../../pi/widget/forelet';
 import { addWidget } from '../../pi/widget/util';
 import { getLocalStorage } from '../utils/tools'
+import { BTCWallet, UTXO, Output } from '../core/btc/wallet';
 // ============================== 导出
 
 export const forelet = new Forelet();
@@ -25,6 +26,33 @@ export const run = (): void => {
 	// popNew('app-view-groupwallet-groupwallet');
 	// popNew('app-view-financialManagement-home');
 	 //popNew('app-view-mine-changePassword-changePassword1');
+
+	// let words = "void chuckle melody have hood veteran face wine cat control thumb wheel admit mammal cinnamon"
+	// let w = BTCWallet.fromMnemonic(words, "testnet", "english");
+	// let utxo = {
+	// 	"txid" : "14067431e8c7f685a68c89c0428fba53705a7d804ea984d60baf899dec7a0121",
+	// 	"vout" : 0,
+	// 	"address" : "mzJ1AAKQpMj5eaCL3b4oNuSantXmVgz2tM",
+	// 	"scriptPubKey" : "76a914cdf75f817ef312950719f6fa1b947e75ab792d2688ac",
+	// 	"amount" : 0.275
+	// };
+	// let output = {
+	// 	toAddr: "n38uMS8K3sM1PfypMd55YH8U4pUrSF4Jqo",
+	// 	amount: 100000,
+	// 	chgAddr: "mzJ1AAKQpMj5eaCL3b4oNuSantXmVgz2tM"
+	// }
+	// console.log(w.derive(0));
+	// console.log(w.derive(1));
+	// console.log(w.derive(2))
+	// console.log(w.signRawTransaction(utxo, output,  0));
+
+	// console.log(BTCWallet.generate("", 128, "testnet", "english", ""))
+
+	// console.log(w)
+	// console.log(w.derive(0))
+	// console.log(w.exportWIFOf(4));
+
+	// console.log(w.toSeed("english", words, ""))
 };
 
 // ============================== 立即执行
